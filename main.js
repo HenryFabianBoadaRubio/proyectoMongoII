94339af43e5de10f4cff86cf5c33f6ec3065855a
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-24 16:51:07 -0500

    feat: :sparkles: Se termina el caso de uso 1 la primera parte.
    Se realiza la configuracion de la primera parte del caso de uso 1 del taller.

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 
 import {connect} from "../proyectoMongoII/helpers/db/connect.js";
+import { pelicula } from "./js/modules/pelicula.js";
 import {practica} from './js/modules/practica.js'
 
 
-let objPractica = new practica();
+// let objPractica = new practica();
+// console.log(`Probando conexion con la db`, await objPractica.getAllTest());
+// objPractica.destructor();
 
-console.log(`Probando conexion con la db`, await objPractica.getAllTest());
 
-objPractica.destructor();
+let objPelicula;
+objPelicula= new pelicula();
+
+//Selección de Películas:
+//parte 1: 
+console.log(await objPelicula.getAllMoviesProjection());
+objPelicula.destructor();
