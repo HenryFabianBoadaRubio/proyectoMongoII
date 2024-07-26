f17caf11ef26fd9e756f4403ab911ce00591da99
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-26 00:47:40 -0500

    feat: :construction: Se continua la construccion del caso de uso dos de boletos.
    Se ingresan diferentes validaciones para la compra de un boleto como lo son  existencia de usuario, pelicula, sala, asientos, proyecciones .

--- js/modules/boleto.js
+++ js/modules/boleto.js
@@ -24,12 +24,20 @@ export class boleto extends connect {
         await this.conexion.close();
         return res;
     }
-
-    async registerBuyTicket({proyeccion_id,usuario_id,asientos,precio_total,descuento_aplicado}) {
+    // ,{proyeccion_id,usuario_id,asientos,precio_total,descuento_aplicado}
+    async registerBuyTicket(id) {
         let res;
         try {
+            //verificar la existencia de la pelicula
+            let peliExist=await this.db.collection('pelicula').findOne({_id: new ObjectId(id._id)})
+            if(!peliExist){
+                return {
+                    error: "Not found",
+                    message: "La pelicula no existe."
+                }
+            }
             // verificar la existencia de usuario 
-            let userExist=await this.db.collection('usuario').findOne({_id: new ObjectId(usuario_id)})
+            let userExist=await this.db.collection('usuario').findOne({_id: new ObjectId(id.usuario_id)})
             if(!userExist){
                 return {
                     error: "Not found",
@@ -38,30 +46,81 @@ export class boleto extends connect {
             }
 
             // verificar la existencia de la proyeccion
-            let proExist= await this.db.collection('proyeccion').findOne({_id: new ObjectId(proyeccion_id)})
+            let proExist= await this.db.collection('proyeccion').findOne({_id: new ObjectId(id.proyeccion_id)})
             if (!proExist){
                 return {
                     error: "Not found",
                     message: "La proyeccion no existe."
                 };
             }
-            //verificar la existencia de asientos.
-            let asientoExist=await this.db.collection('asiento').findOne({fila:fila,numero:numero})
-            if (!asientoExist){
+
+            //verificar que la proyeccion sea de la pelicula
+            if(!proExist.pelicula_id === peliExist._id){
                 return {
                     error: "Not found",
-                    message: "El asiento no existe."
+                    message: "La proyeccion no es de la pelicula."
+                };
+            }
+           
+            //verificar si la sala existe.
+            let salaExist=await this.db.collection('sala').findOne({_id: proExist.sala_id})
+            if (!salaExist) {
+                return {
+                    error: "Not found",
+                    message: "La sala no existe."
+                };
+            }
+            //verificar la disponibilidad de los asientos de la sala 
+           // Obtiene los asientos correspondientes al arreglo de asientos en salaExist
+            const asientosIds = salaExist.asientos.map(asiento => asiento.asiento_id);
+           
+            // Consulta la base de datos para obtener el estado de cada asiento
+            const asientosDisponibles = await Promise.all(asientosIds.map(async (id) => {
+                return this.db.collection('asiento').findOne({ _id: id });
+            }));
+            // Verifica si todos los asientos están disponibles
+            const todosDisponibles = asientosDisponibles.every(asiento => asiento.estado === "disponible");
+            
+            if (!todosDisponibles) {
+                return {
+                    error: "Not found",
+                    message: "Algunos asientos no están disponibles"
                 };
             }
 
+
+            //verificar si el usuario es VIP
+           let usuarioVip = await this.db.collection('tarjetaVIP').findOne({usuario_id: userExist._id})
+           if (!usuarioVip){
+            return {
+                error: "Not found",
+                message: "El usuario no tiene tarjeta VIP."
+            };
+           }
+
             let nuevoBoleto={
-                proyeccion_id: new ObjectId(proyeccion_id),
-                usuario_id: new ObjectId(usuario_id),
-                asientos:asientos,
-                precio_total:precio_total,
-                descuento_aplicado:descuento_aplicado
+                proyeccion_id: new ObjectId(id.proyeccion_id),
+                usuario_id: new ObjectId(id.usuario_id),
+                asientos:id.asientos,
+                precio_total:id.precio_total,
+                descuento_aplicado:id.descuento_aplicado
             }
-            await this.db.collection('boleto').u
+
+
+              res= await this.collection.insertOne(nuevoBoleto);
+            
+              //cambia el estado de los asientos que se ocupen 
+              await Promise.all(asientosIds.map(async (id) => {
+                await this.db.collection('asiento').updateOne(
+                    { _id: id },
+                    { $set: { estado: "ocupado" } }  // Cambia el estado a "ocupado"
+                );
+            }));
+
+              return {
+                    message: "Boleto registrado correctamente.",
+                    boleto_id: res.insertedId
+                };
           
             
         } catch (error) {
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { ObjectId } from "mongodb";
 import {connect} from "../proyectoMongoII/helpers/db/connect.js";
 import { pelicula } from "./js/modules/pelicula.js";
 import {practica} from './js/modules/practica.js'
+import { boleto } from "./js/modules/boleto.js";
 
 
 // let objPractica = new practica();
@@ -10,8 +11,8 @@ import {practica} from './js/modules/practica.js'
 // objPractica.destructor();
 
 /////////················································CASO DE USO 1 Selección de Películas:······································///////////
-let objPelicula;
-objPelicula= new pelicula();
+// let objPelicula;
+// objPelicula= new pelicula();
 
 //parte 1 Listar Películas: 
 // console.log(await objPelicula.getAllMoviesProjection());
@@ -19,8 +20,21 @@ objPelicula= new pelicula();
 
 
 //Parte 2 Obtener Detalles de Película:
-console.log(await objPelicula.getAllMovieInformation(new ObjectId("66a12e9a1219e115c8e79e89")));
+// console.log(await objPelicula.getAllMovieInformation(new ObjectId("66a12e9a1219e115c8e79e89")));
 
-objPelicula.destructor();
-/////////···············································FINAL Selección de Películas:······································///////////
+// objPelicula.destructor();
+// /////////···············································FINAL Selección de Películas:······································///////////
 
+
+
+let objBoleto;
+objBoleto= new boleto()
+
+console.log(await objBoleto.registerBuyTicket({
+    _id:"66a12e9a1219e115c8e79e89",
+    proyeccion_id:"66a12e9b1219e115c8e79e95",
+    usuario_id: "66a12e9b1219e115c8e79e9b",
+    asientos: [{fila: "A", numero: 1}],
+    precio_total: 100,
+    descuento_aplicado: 50
+}));
