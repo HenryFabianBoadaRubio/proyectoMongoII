ebfc59006c2d06d3d6cc50cd56223894371b0752
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-28 16:50:33 -0500

    feat: :sparkles: Se realiza el caso de uso 3 parte 1 de reservar asientos.
    Se realizan las validaciones pertinentes de usuario y proyeccion para generar la reserva de manera correcta.

--- js/modules/boleto.js
+++ js/modules/boleto.js
@@ -193,4 +193,78 @@ export class boleto extends connect {
             console.error("Error en getAvailableSeats:", error);
             throw error;
         }
-    }}
+    }
+
+
+    async reserveSeats({proyeccion_id,usuario_id,asientos,}){
+        let res;
+        try { 
+             //verificar la existencia de la proyeccion
+            let proExist=await this.db.collection('proyeccion').findOne({_id: new ObjectId(proyeccion_id)})
+            if (!proExist){
+                return {
+                    error: "Not found",
+                    message: "La proyeccion no existe."
+                };
+            }
+            //verificar la existencia de usuario.
+            let userExist=await this.db.collection('usuario').findOne({_id: new ObjectId(usuario_id)})
+            if(!userExist){
+                return {
+                    error: "Not found",
+                    message: "El usuario no existe."
+                };
+            }
+          
+           //verificar si la sala existe
+            let salaExist=await this.db.collection('sala').findOne({_id: proExist.sala_id})
+            if (!salaExist) {
+                return {
+                    error: "Not found",
+                    message: "La sala no existe."
+                };
+            }
+            //verificar la disponibilidad de los asientos de la sala 
+            for (let asiento of asientos){
+                let asientoExist=await this.db.collection('asiento').findOne({fila:asiento.fila,numero:asiento.numero, sala_id: salaExist._id, estado: "disponible"})
+                if(!asientoExist){
+                    return {
+                        error: "Not found",
+                        message: "el asiento no está disponible O no existe. ",
+                        asiento:asiento
+                    }
+                }
+            }
+            let precio_total= proExist.precio*asientos.length;
+            let descuentoUser= 0;
+            //verificar si el usuario es VIP y aplica el descuento en caso de serlo 
+            let cardVip = await this.db.collection('tarjetaVIP').findOne({usuario_id: userExist._id})
+            if (cardVip){
+                precio_total=(proExist.precio - cardVip.descuento)*asientos.length;
+                descuentoUser= cardVip.descuento*asientos.length;
+                
+            } 
+            // registrar el nuevo boleto de reserva
+            let nuevoBoleto={
+                proyeccion_id: new ObjectId(proyeccion_id),
+                usuario_id: new ObjectId(usuario_id),
+                asientos:asientos,
+                precio_total:precio_total,
+                descuento_aplicado:descuentoUser
+            }
+
+             res= await this.collection.insertOne(nuevoBoleto);
+             for (let asiento of asientos){
+                await this.db.collection('asiento').updateOne({fila:asiento.fila,numero:asiento.numero,},{$set:{estado: "reservado"}});
+              }
+              return {
+                    message: "Boleto reservado correctamente.",
+                    boleto_id: res.insertedId,
+                    descuento:(`Descuento aplicado: ${descuentoUser}`)
+                };
+        } catch (error) {
+            return { error: "Error", message: error.message,details: error.errInfo};
+            
+        }
+    }
+}
