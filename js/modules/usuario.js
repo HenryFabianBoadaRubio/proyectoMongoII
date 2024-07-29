790f00f309f9de9d5c33b9d7dced900d47125e96
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-28 22:28:18 -0500

    feat: :sparkles: Caso de uso 5 parte 1 Crear Usuario.
    Se genera la validacion de usuario mediante el nombre y se valida que el correo ingresado sea valido para la creacion.
    
    docs: :memo: Se ingresan la documentacion del metodo del caso de uso 5  primera parte.
    
    Se ingresa la documentacion correspondiente al caso de uso 5 primera parte al README
    
    feat: :recycle: Agregamos la validacion de que el correo no exista previamente en el caso de uso 5 parte 1.
    
    Se genera la validacion de correo ya existente para evitar duplicidad en los usuarios.

--- js/modules/usuario.js
+++ js/modules/usuario.js
@@ -24,4 +24,73 @@ export class usuario extends connect {
         await this.conexion.close();
         return res;
     }
+        /**
+     * Registra un nuevo usuario en la base de datos.
+     *
+     * @param {Object} usuario - El objeto de usuario que contiene la información del usuario.
+     * @param {string} usuario.nombre - El nombre del usuario.
+     * @param {string} usuario.email - El correo electrónico del usuario.
+     * @param {string} usuario.rol - El rol del usuario.
+     * @param {string} usuario.nick - El nombre de usuario (nick).
+     *
+     * @returns {Object} - Objeto con el resultado del registro.
+     * @returns {Object.error} - Si hay un error, este campo contendrá el string "Error".
+     * @returns {Object.message} - Mensaje de éxito o error.
+     * @returns {Object.user_id} - ID del usuario registrado (en caso de éxito).
+     * @returns {Object.details} - Detalles adicionales del error (en caso de error).
+     */
+    async registerUser({nombre,email,rol,nick}){
+        try {
+            
+            //verificar la existencia de usuario por nickname
+            let userExist=await this.db.collection('usuario').findOne({nick:nick})
+            if (userExist){
+                return {
+                    error: "Error",
+                    message: "El nick ya existe."
+                };
+            }
+           
+            //validar que el gmail sea correcto
+            let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if(!regex.test(email)){
+                return {
+                    error: "Error",
+                    message: "El email no es válido."
+                };
+            }
+
+            //validar que el gmail no exista ya en la base de datos
+            let userExistEmail=await this.db.collection('usuario').findOne({email:email})
+            if (userExistEmail){
+                return {
+                    error: "Error",
+                    message: "El email ya existe."
+                };
+            }
+            
+        
+
+            const usuario ={
+                nombre:nombre,
+                email:email,
+                rol:rol,
+                nick:nick
+            }
+            const guardaUsuario = await this.db.collection('usuario').insertOne(usuario);
+            await this.db.command({
+                createUser: nick,
+                pwd: guardaUsuario.insertedId.toString(),
+                roles: [{role: rol, db: 'cineCampus'}]
+
+            })
+            return {
+                message: "Usuario registrado correctamente.",
+                user_id: guardaUsuario.insertedId
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message,details: error.errInfo};
+            
+        }
+    }
 }
