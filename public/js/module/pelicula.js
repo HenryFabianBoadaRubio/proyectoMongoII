3945db83d8d9b10886662135dbb242bdcc70cfee
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-08-12 13:14:19 -0500

    feat: :sparkles: Se realiza la insercion de proyecciones en la web

--- public/js/module/pelicula.js
+++ public/js/module/pelicula.js
@@ -1,3 +1,6 @@
+// const {ObjectId} =require('mongodb');
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const peliculaId = urlParams.get('peliculaId');
@@ -8,9 +11,26 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch(`http://localhost:5001/pelicula/unaPelicula/${peliculaId}`)
         .then(response => response.json())
         .then(data => { 
+            console.log('Detalle de la pelicula', data);
+            fetch(`http://localhost:5001/pelicula/todasPeliculas`)
+            .then(response => response.json())
+            .then(allData=>{
+                // const projectionMovie = allData.find(movies => movies._id.equals(new ObjectId(peliculaId)));
+                const projectionMovie = allData.find(movies => movies._id.toString() === peliculaId);
+                if(projectionMovie){
+                    console.log('proyecciones encontradas:', projectionMovie.proyecciones);
+                    
+                }else{
+                    console.error('NO se encontraron proyecciones',peliculaId)
+                }
             displayMovieDetail(data);
+            displayProjectionMovie(projectionMovie);
         })
-        .catch(error => console.error('Error al cargar detalles de la película:', error));
+        .catch(error => console.error('Error al cargar proyecciones de la película:', error));
+    })
+    .catch(error=> console.error('Error al cargar detalles', error))
+    
+
 });
 
 function displayMovieDetail(peliculas) {
@@ -25,7 +45,55 @@ function displayMovieDetail(peliculas) {
             <p><strong>Género:</strong> ${pelicula.genero}</p>
             <p><strong>Duración:</strong> ${pelicula.duracion} minutos</p>
             <p><strong>Actores:</strong> ${pelicula.actores}</p>
+            
         </div>    
      </div>   
     `;
+}
+// function displayProjectionMovie(peliculas) {
+//     console.log('Objeto pelicula en displayProjectionMovie:', peliculas);
+//     const containers = document.getElementById('carrusel__proyecciones');
+//     console.log('Pelicula:', peliculas);
+//     console.log('Proyecciones:', peliculas.proyecciones);
+
+
+//     if (peliculas && peliculas.proyecciones) {
+//         console.log('Proyecciones:', peliculas.proyecciones);
+
+//     const proyecciones = Array.isArray(peliculas.proyecciones) ? peliculas.proyecciones : [];
+//     const proyeccionesHtml = peliculas.proyecciones.map(proyeccion => {
+//         /
+//         return `<li>Fecha: ${proyeccion.fecha}, Sala: ${proyeccion.sala_id.nombre}</li>`;
+//     }).join('');
+
+//     containers.innerHTML = `
+//     <div class="movie_container__detalle">
+//         <ul>${proyeccionesHtml}</ul>
+//     </div>
+//     `;
+// }}
+function displayProjectionMovie(peliculas) {
+    console.log('Objeto pelicula en displayProjectionMovie:', peliculas);
+    const containers = document.getElementById('carrusel__proyecciones');
+    
+    if (peliculas && peliculas.proyecciones) {
+        console.log('Proyecciones:', peliculas.proyecciones);
+        
+        
+        const proyeccionesHtml = peliculas.proyecciones.map((proyeccion, index) => {
+            return `<li class="proyeccion-item -${index}">
+                        <span class="proyeccion__fecha">Fecha: ${proyeccion.fecha}</span>,
+                        <span class="proyeccion__precio">Precio: ${proyeccion.precio}</span>,
+                        <span class="proyeccion__formato">Formato: ${proyeccion.formato}</span>
+                    </li>`;
+        }).join('');
+
+        containers.innerHTML = `
+        <div class="movie__container__proyecciones">
+            <ul>${proyeccionesHtml}</ul>
+        </div>
+        `;
+    } else {
+        containers.innerHTML = '<p>No hay proyecciones disponibles</p>';
+    }
 }
