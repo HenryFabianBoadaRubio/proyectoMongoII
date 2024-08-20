2a1b2339247401b01f10fb74bb0b08062dd36a62
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-08-19 22:31:50 -0500

    feat: :construction: Se agrega funcionalidad al contador para acabar con el proceso de compra si se tarda mas de lo permitido.
    Se realiza la configuracion de funcionamiento y disposicion de estilo de la web de compra

---
+++ public/js/module/pago.js
@@ -0,0 +1,25 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const countdownElement = document.getElementById('countdown');
+//     // let timeLeft = 4 * 60 + 59; // 4 minutos y 59 segundos en segundos
+//     let timeLeft =  9;
+//     function updateCountdown() {
+//         const minutes = Math.floor(timeLeft / 60);
+//         const seconds = timeLeft % 60;
+        
+//         countdownElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        
+//         if (timeLeft === 0) {
+//             clearInterval(countdownInterval);
+//             // Redirigir a la página anterior
+//             window.history.back();
+//         } else {
+//             timeLeft--;
+//         }
+//     }
+
+//     // Actualizar el contador cada segundo
+//     const countdownInterval = setInterval(updateCountdown, 1000);
+
+//     // Iniciar el contador inmediatamente
+//     updateCountdown();
+// });
