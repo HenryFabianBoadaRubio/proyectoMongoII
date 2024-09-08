df2f299cb1c4afdba3fff8cbf2d653f30d5e454c
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-09-08 13:45:49 -0500

    feat: :sparkles: Se agrega docker para poder desplegar el proyecto

--- app.js
+++ app.js
@@ -143,9 +143,14 @@ app.use((err, req, res, next) => {
  });
  
 
+ const config = {
+    port: process.env.EXPRESS_PORT || 5001,
+    host: process.env.EXPRESS_HOST || '0.0.0.0',
+    static: process.env.EXPRESS_STATIC || 'public'  
+};
 
 
-
-app.listen({host: process.env.EXPRESS_HOST, port: process.env.EXPRESS_PORT}, () => {
+app.listen(config.port,'0.0.0.0', () => {
     console.log(`http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`);
 })
+
